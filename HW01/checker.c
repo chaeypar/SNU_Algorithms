16920a057761ec5438c9184f86d3cf320a852801
chaeyeon <kidsland09@naver.com>
2023-03-28 15:16:46 +0900

    modified

--- HW01/checker.c
+++ HW01/checker.c
@@ -2,20 +2,22 @@
 
 int main(int argc, char *argv[]){
 
-    int N, M, smallcnt=0, samecnt=0, num, target;
+    int N, M, smallcnt=0, samecnt=0, num, target, c;
 
-    //There should be four arguments, i.e. executable object file, input file and output file.
+    //There should be three arguments, i.e. executable file, input file and output file.
     if (argc!=3){
         printf("There should be three arguments. Check once again.\n");
         return -1;
     }
 
+    //Open the input file. If there are any errors, then print the error message and terminate the program.
     FILE *fpin=fopen(argv[1], "r");
     if (!fpin){
         printf("Failure to open the input file. Program is terminated.\n");
         return -1;
     }
     
+    //Open the output file. If there are any errors, then print the error message and terminate the program.
     FILE *fpout=fopen(argv[2], "r");
     if (!fpout){
         printf("Failure to open the output file. Program is terminated.\n");
@@ -23,23 +25,31 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    
-    fscanf(fpout, "%d", &target);
-    fscanf(fpin, "%d", &N);
-    fscanf(fpin, "%d", &M);
+    //We should check whether the integer in the output file is the correct answer for the given input file.
+    c = fscanf(fpout, "%d", &target);
+    c = fscanf(fpin, "%d", &N);
+    c = fscanf(fpin, "%d", &M);
 
+    //If each integer in the given input file is smaller than 'target', then increment the 'smallcnt' by 1.
+    //If each integer in the given input file is equal to 'target', then increment the 'samecnt' by 1.
     for (int i=0;i<N;i++){
-        fscanf(fpin, "%d", &num);
+        c = fscanf(fpin, "%d", &num);
         if (num<target)
             smallcnt++;
         else if (num==target)
             samecnt++;
     }
+
+    //If smallcnt is 7 and samecnt is 3, it means that there are 7 integers smaller than 'target' and 3 integers equal to 'target'.
+    //Therefore, 'target' is the 8(=7+1)th smallest, the 9(=7+2)th smallest, and the 10(=7+3)th smallest value.
+    //Hence, we should check whether M is greater than M and less than or equal to (smallcnt + samecnt). 
+    //If it is, we should print 1 to stdout. Otherwise, we should print 0 to stdout.
     if (M>smallcnt&&M<=smallcnt+samecnt)
         printf("1\n");
     else
         printf("0\n");
 
+    //Close the given input and output files.
     fclose(fpin);
     fclose(fpout);
 
--- HW01/main.c
+++ HW01/main.c
@@ -5,34 +5,37 @@
 
 int arr[MAXNUM];
 
-int rpartition(int p, int r);
+int partition(int p, int r);
 int randomized_select(int p, int r, int i);
 void insertion_sort(int p, int r);
 int deterministic_select(int p, int r, int i);
 
 int main(int argc, char *argv[]){
 
-    int N, M, mode, ans;
+    int N, M, mode, ans, c;
     time_t start, end;
 
-    //There should be four arguments, i.e. executable object file, mode, input file and output file.
+    //There should be four arguments, i.e. executable file, mode, input file and output file.
     if (argc!=4){
         printf("There should be four arguments. Check once again.\n");
         return -1;
     }
 
+    //If argv[1] is not 1 or 2, then it is not valid.
     mode=atoi(argv[1]);
     if (mode!=1 && mode!=2){
         printf("The mode is invalid. It should be either 1 or 2.\n");
             return -1;
     }
 
+    //Open the input file. If there exist any errors, then terminate the program.
     FILE *fpin=fopen(argv[2], "r");
     if (!fpin){
         printf("Failure to open the input file. Program is terminated.\n");
         return -1;
     }
     
+    //Open the output file. If there exist any errors, then terminate the program.
     FILE *fpout=fopen(argv[3], "wb");
     if (!fpout){
         printf("Failure to open the output file. Program is terminated.\n");
@@ -40,12 +43,15 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    fscanf(fpin, "%d", &N);
-    fscanf(fpin, "%d", &M);
+    //Store the values of N, M and input array to the variables from input file.
+    c = fscanf(fpin, "%d", &N);
+    c = fscanf(fpin, "%d", &M);
     for (int i=0;i<N;i++){
-        fscanf(fpin, "%d", &arr[i]);
+       c = fscanf(fpin, "%d", &arr[i]);
     }
 
+    //If mode=1, we should execute randomized selection algorithm.
+    //If mode=2, we shoudl execute deterministic selection algorithm.
     switch (mode){
         case 1:
             start = clock();
@@ -59,16 +65,18 @@ int main(int argc, char *argv[]){
             break;
     }
     fprintf(fpout, "%d\n", ans);
-    printf("Programming running time : %ldms\n", (end-start)*1000/CLOCKS_PER_SEC);
+    printf("Total running time : %lfs\n", (double)(end-start)/CLOCKS_PER_SEC);
 
     fclose(fpin);
     fclose(fpout);
     return 0;
 }
 
+//partition algorithm in the lecture note. It sets arr[r] as a pivot.
 int partition(int p, int r){
     int x, i, temp;
 
+    //Set arr[r] as a pivot.
     x=arr[r];
     i=p-1;
     for (int j=p;j<r;j++){
@@ -83,15 +91,15 @@ int partition(int p, int r){
     arr[i+1]=arr[r];
     arr[r]=temp;
     return i+1;
-
 }
 
+//Randomized_selection algorithm
 int randomized_select(int p, int r, int i){
     int q, k;
 
     if (p==r)
         return arr[p];
-    q = partition(p, r);
+    q = partition(p, r); 
     k = q-p+1;
     if (i==k)
         return arr[q];
@@ -101,6 +109,7 @@ int randomized_select(int p, int r, int i){
         return randomized_select(q+1,r,i-k);
 }
 
+//Insertion sort on the array = (arr[p], arr[p+1], ..., arr[r])
 void insertion_sort(int p, int r){
     int x, j;
     for (int i=p+1;i<=r;i++){
@@ -114,20 +123,29 @@ void insertion_sort(int p, int r){
     }
 }
 
+//Modified version of partition() function. 
 int partition_with_pivot(int p, int r, int pividx){
+    //Swap arr[pividx] and arr[r] at first.
+    //Note that pivot is now equal to arr[r].
     int temp=arr[pividx];
     arr[pividx]=arr[r];
     arr[r]=temp;
+    
+    //Then call partition(p, r) since arr[r] is a pivot. 
     return partition(p, r);
 
 }
 
+//Deterministic selection algorithm
 int deterministic_select(int p, int r, int i){
+    //If n<=5, find the answer by insertion sort and return
     if (r-p+1<=5){
         insertion_sort(p, r);
         return arr[p+i-1];
     }
 
+    //Divide n elements into groups of 5 elements.
+    //gcnt means the number of the groups.
     int gcnt=(r-p+1)/5;
     if ((r-p+1)%5)
         gcnt++;
@@ -135,6 +153,8 @@ int deterministic_select(int p, int r, int i){
     int pividx, temp, q, k;
     for (int j=0; j<gcnt; j++){
         int gnum=r-(p+j*5)+1;
+        //find median in eaach group by insertion sort.
+        //let m_1, m_2, ..., m_gcnt be these medians.
         if (gnum < 5){
             insertion_sort(p+5*j, r);
             temp = arr[p + j];
@@ -149,15 +169,22 @@ int deterministic_select(int p, int r, int i){
         }
     }
 
+    //find median M of m_1, m_2, ..., m_gcnt recursively.
     int median = deterministic_select(p, p + gcnt - 1, (gcnt+1)/2);
+ 
+    //find the index of the medain in the 'arr' array.
     for (int j = p; j < p + gcnt ; j++){
         if (arr[j]==median){
             pividx=j;
             break;
         }
     }
+
+    //partition n elements using median as pivot.   
     q = partition_with_pivot(p, r, pividx);
     k = q - p + 1;
+    
+    //same as randomized_select()
     if (i==k)
         return arr[q];
     else if (i<k)
